98ff1b6e91578c36a0f5bc316123676b6b1305cf
Ayara Panmella de Assis Dantas <20261016270050@ifrn.local>
2026-06-10 22:01:23 -0300

    primeiro commit

---
+++ catalogo/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    obras = [
+        {
+            "id": 1,
+            "titulo": "interestelar",
+            "tipo": "filme",
+        },
+        {
+            "id": 2,
+            "titulo": "jujuland",
+            "tipo": "serie",
+        }
+
+    ]
+    contexto = {
+        "obras": obras,
+
+    }
+    
+    return render(request, "catalogo/index.html", contexto)
